2f8529fb4151edf10e4be4c4ba55b222ffa0caee
Chris <chris.barwick16@gmail.com>
2021-11-23 23:15:10 -0800

    [test] CI / Coverage update

--- src/main/java/panda/metricbot/Bot.java
+++ src/main/java/panda/metricbot/Bot.java
@@ -2,6 +2,7 @@ package panda.metricbot;
 
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import panda.metricbot.activities.BotActivity;
 import panda.metricbot.exceptions.InvalidTokenException;
 
 import javax.security.auth.login.LoginException;
@@ -12,40 +13,11 @@ import javax.security.auth.login.LoginException;
 public class Bot {
 
 	/**
-	 * Single constructor for building the bot application.
-	 * @param token
-	 */
-	public Bot(String token) {
-		try {
-			loginBot(token);
-		} catch (LoginException loginException) {
-			System.out.println("Failed to login with provided token.");
-			throw new InvalidTokenException(
-					String.format("The provided token is invalid.\nToken: %s", token),
-					loginException);
-		}
-	}
-
-	/**
 	 * Entry point for Bot application. Args must start with TOKEN.
 	 * @param args - The args provided for the Bot application,
 	 *             where the first arg is the bot TOKEN.
 	 */
 	public static void main(String[] args) {
-		new Bot(args[0]);
-	}
-
-	/**
-	 * Attempts to login the bot application.
-	 * @param token - The token for the bot to login with.
-	 * @throws LoginException
-	 */
-	public void loginBot(String token) throws LoginException {
-		JDABuilder builder = JDABuilder.createDefault(token);
-
-		// Set activity ("watching Metrics")
-		builder.setActivity(Activity.watching("Metrics."));
-
-		builder.build();
+		new BotActivity(args[0]);
 	}
 }
